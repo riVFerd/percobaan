a8213ce18c9cfaac5b6ed641a1ee89bfcea125d1
riVFerd <virgyferdian213@gmail.com>
2021-11-04 12:08:03 +0700

    percobaan sebelumnya diganti dengan percobaan program namaBulan

--- Percobaan.java
+++ Percobaan.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
 
-//percobaan program menampilkan nilai dari array secara kebalik
+//percobaan program menampilkan nama nama bulan
 public class Percobaan {
     public static void main(String[] args) {
+        //preparation
         Scanner sc = new Scanner(System.in);
-        int[] numbers = new int[5];
+        String[] namaBulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", 
+        "September", "Oktober", "November", "Desember"};
+        System.out.print("Masukkan angka 1-12: ");
+        int angka = sc.nextInt();
 
-        //input loop
-        for (int i=0; i<numbers.length; i++) {
-            System.out.print("Masuukan nilai indeks ke-" + i + ": ");
-            numbers[i] = sc.nextInt();
-        }
-        sc.close();
-
-        //print array with loop
-        for (int i=numbers.length-1; i>=0; i--) {
-            System.out.printf("%s%d : %d\n", "Nilai dari indeks ke-", i, numbers[i]);
-        }
+        //print
+        System.out.println(namaBulan[angka-1]);
     }
 }
