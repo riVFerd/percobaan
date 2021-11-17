c5e15beae8f528ac1e4b3894bb96b608b5329ff1
riVFerd <virgyferdian213@gmail.com>
2021-11-17 14:44:26 +0700

    percobaan untuk tugas CT nomor 1 pertemuan 12

--- Percobaan.java
+++ Percobaan.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
-//percobaan program menampilkan nama nama bulan
+//percobaan program 
 public class Percobaan {
     public static void main(String[] args) {
-        //preparation
+        double keuntungan,banyakPekerja, pendapatanPot, potTerjual, pengeluaranPot;
         Scanner sc = new Scanner(System.in);
-        String[] namaBulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", 
-        "September", "Oktober", "November", "Desember"};
-        System.out.print("Masukkan angka 1-12: ");
-        int angka = sc.nextInt();
 
-        //print
-        System.out.println(namaBulan[angka-1]);
+        System.out.print("Masukkan banyaknya pekerja: ");
+        banyakPekerja = sc.nextDouble();
+
+        pendapatanPot = banyakPekerja*20_000;
+        potTerjual = banyakPekerja*5_000;
+        pengeluaranPot = (potTerjual/10_000)*1_000;
+        double pengeluaran = 15_000;
+        double bayarStaff = 85_000;
+
+        keuntungan = pendapatanPot - pengeluaran - pengeluaranPot - bayarStaff;
+        System.out.println("Jumlah keuntungan pertahun = " + keuntungan);
     }
 }
