a0d0a9998c638e4f124ed6990147241a48354184
riVFerd <virgyferdian213@gmail.com>
2021-12-02 10:15:46 +0700

    replace file experimental2 with program to find fpb/hcf

--- Experimental2.java
+++ Experimental2.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
+//find fpb/hcf with simple logic by me (with recursive method)
 public class Experimental2 {
-    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        kaliDua(sc.nextInt());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan angka pertama: ");
+        int n1 = sc.nextInt();
+        System.out.println("Masukkan angka pertama: ");
+        int n2 = sc.nextInt();
+        
+        System.out.printf("Fpb dari %d dan %d adalah %d", n1, n2, fpb(n1, n2, n2));
     }
     
-    static void kaliDua(int num) {
-        int result = num * 2;
-        System.out.println("Hasil kali dua dari "+num+" adalah "+result);
+    static int fpb(int n1, int n2, int i) {
+        if (n1%i == 0 && n2%i == 0) {
+            return i;
+        } else {
+            return fpb(n1, n2, i-1);
+        }
     }
 }
